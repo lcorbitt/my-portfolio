35f303c6067e7128131df60755ea16b9ce3d9fa7
Lukas Corbitt <movalley5@gmail.com>
2025-06-19 12:08:06 -0600

    Linting

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -99,9 +99,9 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* form side */}
           <div className="flex flex-col w-full xl:w-1/2 bg-[#27272c] p-12 rounded-xl">
-            <h2 className="text-4xl mb-8 text-accent">Let's work together</h2>
+            <h2 className="text-4xl mb-8 text-accent">Let&apos;s work together</h2>
             <p className="text-gray-400 mb-12">
-              Have a project in mind? Let's discuss how we can work together to bring your ideas to life.
+              Have a project in mind? Let&apos;s discuss how we can work together to bring your ideas to life.
             </p>
 
             <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col gap-y-8">
